10c858b9fb2e412c0a4f045c65535cc84be99e10
Yixuan Wu <ywu33@computerart.net>
2020-01-28 14:55:41 -0500

    what did I do this time

---
+++ Assets/script/MoveAnObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnObject : MonoBehaviour
+{
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 jiggle = new Vector3(
+            position.x,
+            position.y + Random.value,
+            position.z);
+        this.transform.position = jiggle;
+        this.transform.localEulerAngles = rotation;
+        this.transform.
+    }
+}
---
+++ Assets/script/RulesBasedGeometryPlacer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulesBasedGeometryPlacer : MonoBehaviour
+{
+    public GameObject thing;
+
+    public Vector3 position;
+    public Vector3 scale = Vector3.one;
+    public Vector3 rotation;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        thing.transform.position = position;
+        thing.transform.localScale = scale;
+        thing.transform.eulerAngles = rotation;
+    }
+}
