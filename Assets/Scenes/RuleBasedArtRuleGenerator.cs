75a66c6d8b974e26ec6759da339547961b36515a
Yixuan Wu <ywu33@computerart.net>
2020-01-28 14:03:27 -0500

    message

---
+++ Assets/Scenes/RuleBasedArtRuleGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleBasedArtRuleGenerator : MonoBehaviour
+{
+    public string Greeting = "Hello World";
+
+    public string[] Greetings;
+
+    public int HowMany = 10;
+
+    public float weight = .5f;
+
+    public bool Heavy = false;
+
+    public bool refresh;
+    void Start()
+    {
+        print(Greeting + " "+ HowMany + " " + weight + " " + Heavy);
+
+    }
+
+    void Update()
+    {
+        if (refresh)
+        {
+            int index = Random.Range(0, Greetings.Length);
+            string thisGreeting = Greetings[index];
+            print(thisGreeting);
+
+            refresh = false;
+        }
+    }
+}
